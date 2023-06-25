73bf27bee2f1451f530701d1b1cf4d6ca3aeeec3
Lino <marciooliveira@sistemafieg.org.br>
2023-06-25 11:11:06 -0300

    Inicial

--- .history/app/layout_20230624133758.tsx
+++ .history/app/layout_20230624133758.tsx
@@ -1,8 +1,3 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
-import "./globals.css";
-
 export const metadata = {
    title: "Flexibble",
    description: "Showcase and discover remarkable developer projects",
@@ -11,11 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
       <html lang="en">
-         <body>
-            <Navbar />
-            <main>{children}</main>
-            <Footer />
-         </body>
+         <body>{children}</body>
       </html>
    );
 }
--- .history/app/layout_20230624133846.tsx
+++ .history/app/layout_20230624133846.tsx
@@ -1,6 +1,3 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
 import "./globals.css";
 
 export const metadata = {
@@ -12,9 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
    return (
       <html lang="en">
          <body>
-            <Navbar />
             <main>{children}</main>
-            <Footer />
          </body>
       </html>
    );
