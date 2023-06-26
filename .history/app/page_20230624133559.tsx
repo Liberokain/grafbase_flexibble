a8af185f5eabdf85b32dea2be584a6c31d990f03
Lino <marciooliveira@sistemafieg.org.br>
2023-06-25 21:18:56 -0300

    Ajustes

--- .history/app/page_20230624133559.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-   return (
-      <section className="flest-start flex-col-paddings mb-16">
-         <h1>Home</h1>
-      </section>
-   );
-};
-
-export default Home;
--- components/AuthProviders.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const AuthProviders = (props: Props) => {
-   return <div>AuthProviders</div>;
-};
-
-export default AuthProviders;
